1881549fd7b8c5b9c1c695eee07edd1753b81714
egementunca <egementunca@Egemen-MacBook-Pro.local>
2021-05-08 17:24:27 +0300

    figure creation

--- hesisen1d_analytical.py
+++ hesisen1d_analytical.py
@@ -2,16 +2,14 @@ import numpy as np
 from scipy.special import spherical_jn
 import matplotlib.pyplot as plt
 
-def lamda(l,J):
+def g(l,J):
     
     return 4*np.pi*(1j**l)*spherical_jn(l,-1j*J)
 
-def eigen(J):
-    
-    kappa_steps = []
-    for l in range(100):
-        x = (lamda(l,J)/lamda(0,J))**2
-        kappa_steps.append(x)
-    return kappa_steps, np.sum(kappa_steps)
 
+
+
+x = np.linspace(0,10,500)
+plt.plot(x,-1*np.log(g(0,x)))
+plt.show()
 
