463a2720e8dbe934d51eb21f1035fc5ef7a19bbb
egementunca <egementunca@Egemen-MacBook-Pro.local>
2021-05-24 16:18:47 +0300

    Macro Properties Analytical Calculations and Figures added

--- hesisen1d_analytical.py
+++ hesisen1d_analytical.py
@@ -1,15 +1,21 @@
 import numpy as np
-from scipy.special import spherical_jn
 import matplotlib.pyplot as plt
 
-def g(l,J):
+def g(J,beta):
     
-    return 4*np.pi*(1j**l)*spherical_jn(l,-1j*J)
+    return 4*np.pi*np.sinh(J*beta)*(1/(J*beta))
+
+def fEnergy(J,beta):
+
+    return -1*np.log(g(J,beta))
+
+def der(J,beta):
+
+    eps = 10e-8
+    diff = fEnergy(J,beta+eps)-fEnergy(J,beta)
+    return diff/eps
 
 
-
-
-x = np.linspace(0,10,500)
-plt.plot(x,-1*np.log(g(0,x)))
+x = np.linspace(0.1,100,5000)
+plt.plot(1/x,-1*(x**2)*der(1,der(1,x)))
 plt.show()
-
