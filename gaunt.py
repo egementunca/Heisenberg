18ea66b0a063ee5b55d9af1d98e453603656af76
egementunca <egementunca@Egemen-MacBook-Pro.local>
2021-09-20 17:22:02 +0300

    tested

--- gaunt.py
+++ gaunt.py
@@ -17,42 +17,35 @@ def cleb_coef(l):
 				pass
 	return(np.sum(vals))
 
-a = cleb_coef(1)
-print(a)
-
-def gaunt_coef(l1,l2):
+def gaunt_coef(l,m):
 
 	vals = []
-	count = 0
-	for l3 in range(abs(l2-l1),l1+l2+1):
-		for m1 in range(-l1,l1+1):
-			for m2 in range(-l2,l2+1):
-				for m3 in range(-l3,l3+1):
-					if m1+m2 == m3:
-						if (l1+l2+l3)%2 == 0:
-							count = count+1
-							x = gaunt(l1,l2,l3,m1,m2,-m3,prec=8)*((-1)**m3)
-							print("Gaunt Integral for l3={}, m1={}, m2={}, m3={} is {}".format(l3,m1,m2,m3,x))
-							vals.append(x)
-						else:
-							#print("Gaunt Integral for l3={}, m1={}, m2={}, m3={} cannot be evaluated because of odd L".format(l3,m1,m2,m3))
-							pass
-					else:
-						#print("Gaunt Integral for l3={}, m1={}, m2={}, m3={} cannot be evaluated because of non zero M".format(l3,m1,m2,m3))
-						pass
-	print("Gaunt Integral for l1={} and l2={} summed over {} possible L,Ms".format(l1,l2,count))
-	print(np.sum(vals))
-	return np.sum(vals)
+	for l1 in range(6):
+		for l2 in range(6):
+			for m1 in range(-l1,l1+1):
+				for m2 in range(-l2,l2+1):
+					x = gaunt(l1,l2,l,m1,m2,-m,prec=8)*((-1)**m)
+					print(x)
+					vals.append(x)
+	return vals
 
 def flow_to_excel(flow, file_name):
 
     df_list = []
     writer = pd.ExcelWriter(file_name)
-#len(flow)
-    for i in range(1):
-        df_list.append(pd.DataFrame(flow))
-        df_list[i].to_excel(writer, sheet_name='RG_NO_{}'.format(i+1), float_format='%1.5f')
+    for i in range(len(flow)):
+    	df_list.append(flow[i])
+    df = pd.DataFrame(df_list)
+    df.to_excel(writer, sheet_name='RG', float_format='%1.4f')
     writer.save()
 
     return True
+
+flow = []
+a = gaunt_coef(1, -1)
+print(sum(a))
+#for l in range(6):
+#	for m in range(-l,l+1):
+#		flow.append(gaunt_coef(l, m))
+#flow_to_excel(flow, "gaunt.xlsx")
 
